25922b4bd93d4788bd0e12bc589388cd5d02b97a
Filip Vrba <filipvrba@pm.me>
2022-11-02 13:48:31 +0100

    update scaffold

--- lib/ruby_js/scaffold.rb
+++ lib/ruby_js/scaffold.rb
@@ -11,6 +11,19 @@ module RubyJS
 
       json_oc = JsonParser.new File.join(path_ao, "package.json")
       json_oc.parse :name, project.downcase
+
+      change_server_f(path_ao)
+      install_vite(project, path_ao)
+    end
+
+    def self.change_server_f path_ao
+      path_bin_ao = "#{path_ao}/bin/server"
+      content = Helper.open(path_bin_ao)
+      content_ch = content.sub("APP_NAME", Constants::APP_NAME)
+      Helper.write(path_bin_ao, content_ch)
+    end
+
+    def self.install_vite project, path_ao
       %x(cd #{path_ao} && npm install -D vite)
       puts "\nDone. Now run:\n\n  cd #{project}\n  bin/server\n\n"
     end
