f051ba4c56b37f86ee6cb90e28991d03cba74505
Filip Vrba <filipvrba@pm.me>
2023-06-15 23:06:52 +0200

    added comments in the code which were gererated

--- docs-dev/plugins/docs/lib/docs/generate.rb
+++ docs-dev/plugins/docs/lib/docs/generate.rb
@@ -10,17 +10,27 @@ module RJSV
 
           indexs_comments = Docs::Core::Content.indexs(content, '##><', false)
           index_module = Docs::Core::Content.indexs(content, "module\s.*?><").max
-          indexs_defs = Docs::Core::Content.indexs(content, "def\s(.*?)><").prepend(index_module)
+          indexs_defs = Docs::Core::Content.indexs(content, "def\s(.*?)><")
+          
+          if index_module
+            if !indexs_comments.empty? && index_module < indexs_comments[0]
+              index_module = Docs::Core::Content.indexs(content, "class\s[^>]*><").first
+            end
+             
+            indexs_defs = indexs_defs.prepend(index_module)
+          end
 
           unless indexs_comments.empty? &&
                  indexs_defs.empty?
-            indexs_comments.length.times do |i|
-              comments_raw = content[indexs_comments[i], indexs_defs[i] - indexs_comments[i]]
-                             .gsub(/><\s*/, ' ')
-                             .gsub(/#( ?)/, '')
-                             .strip
-              comments_split = comments_raw.scan(/^(.*)\s\s(.*)$/).last
-              result << comments_split
+              if indexs_comments.length == indexs_defs.length
+                indexs_comments.length.times do |i|
+                comments_raw = content[indexs_comments[i], indexs_defs[i] - indexs_comments[i]]
+                              .gsub(/><\s*/, ' ')
+                              .gsub(/#( ?)/, '')
+                              .strip
+                comments_split = comments_raw.scan(/^(.*)\s\s(.*)$/).last
+                result << comments_split
+              end
             end
           end
 
@@ -42,7 +52,7 @@ module RJSV
 
         def docs_api(input_path)
           files = Dir.glob File.join(input_path, '**', '*.rb')
-          # files = ['/home/filip/Documents/code/ruby-js/lib/rjsv/cli/arguments.rb']
+          # files = ['/home/filip/Documents/code/ruby-js/lib/rjsv/plugin.rb']
           api = []
 
           files.each do |path_file|
@@ -54,6 +64,7 @@ module RJSV
               unless comments_api.empty?
                 content_hash = {
                   file: File.basename(path_file),
+                  path: File.dirname(path_file).sub(File.join(ROOT, ''), ''),
                   comments_with_keywords: comments_api,
                 }
                 
