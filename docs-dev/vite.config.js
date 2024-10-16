ea50dd3568822aeb2cec240a8128b3bddf703229
Filip Vrba <filipvrba@pm.me>
2024-10-16 19:14:26 +0200

    fix path for png

--- docs-dev/vite.config.js
+++ docs-dev/vite.config.js
@@ -15,12 +15,12 @@ export default {
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/rjsv-192x192.png',
+            src: './rjsv-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/rjsv-512x512.png',
+            src: './rjsv-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
