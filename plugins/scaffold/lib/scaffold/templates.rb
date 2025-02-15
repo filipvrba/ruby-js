7a8739a99946224fa071d23046a82630f9a13642
Filip Vrba <filipvrba@pm.me>
2025-02-15 10:18:13 +0100

    update keypress_functions

--- plugins/scaffold/lib/scaffold/templates.rb
+++ plugins/scaffold/lib/scaffold/templates.rb
@@ -15,6 +15,7 @@ module RJSV
           input_variables  = element_attributes[:inputs].map do |h|
             new_hash             = generate_variable_assignment.call(h[:id])
             new_hash[:button_id] = h[:button_id]
+            new_hash[:type]      = h[:type]
 
             new_hash
           end
@@ -84,21 +85,65 @@ module RJSV
           keypress_handlers = input_variables.map do |h|
             input = h[:name]
             keypress_fn = "#{input}_keypress"
-            { template: "@h_#{input}_keypress = lambda { #{keypress_fn}() }", keypress_fn: keypress_fn }
+            { template: "@h_#{input}_keypress = lambda { #{keypress_fn}() }", keypress_fn: keypress_fn, name: input, button_id: h[:button_id] }
+          end
+
+          l_input_from_buttons = lambda do
+            result = {}
+            button_variables.each do |button_var|
+              btn_name = button_var[:name]
+
+              inputs = input_variables.select {|a| a[:button_id].camel_to_snake == btn_name}
+              result[btn_name] = inputs
+            end
+
+            result
           end
         
           generate_keypress_functions = ->(keypress_handlers) do
+            inputs = l_input_from_buttons.call()
+
             keypress_handlers.map do |h|
-              "def #{h[:keypress_fn]}()\n    return unless event.key == 'Enter'\n\n    # TODO: input.focus()\n  end"
-            end.join("\n\n  ")
+              button_inputs        = inputs[h[:button_id].camel_to_snake]
+              index_relevant_input = button_inputs.map.with_index {|hh, i|
+                i if hh[:name] == h[:name]}.join.to_i
+
+              template_focus = '# TODO: input.focus()'
+              if index_relevant_input == button_inputs.length - 1
+                rel_button = button_inputs[index_relevant_input][:button_id].camel_to_snake
+                template_focus = "@#{rel_button}.click()"
+              elsif (index_relevant_input + 1) <= (button_inputs.length - 1)
+                rel_input = button_inputs[index_relevant_input + 1][:name]
+                template_focus = "@#{rel_input}.focus()"
+              end
+
+              "
+  def #{h[:keypress_fn]}()
+    return unless event.key == 'Enter'
+
+    #{template_focus}
+  end"
+            end.join("\n  ")
           end
 
           generate_input_validation_functions = input_variables.map do |h|
             var_name = h[:name]
-            "
+            
+            case h[:type]
+            when 'email'
+              "
+  def have_#{var_name}()
+    email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    @#{var_name}.value.length > 0 &&
+      email_regex.test(@#{var_name}.value)
+  end"
+            else
+              "
   def have_#{var_name}()
     @#{var_name}.value.length > 0
   end"
+            end
           end
 
           template = <<~RUBY
@@ -123,7 +168,6 @@ module RJSV
               end
         
               #{generate_window_function_definitions.call(window_functions)}
-
               #{generate_keypress_functions.call(keypress_handlers)}
               #{generate_input_validation_functions.join("\n")}
             end
